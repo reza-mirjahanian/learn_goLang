ecaf6d88cae6f5094939de91cc677a5f63f6a472
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-21 10:12:25 +0430

    slice

---
+++ examples/arrays.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// In typical Go code, slices are much more common; arrays are useful in some special scenarios.
+func main() {
+
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
